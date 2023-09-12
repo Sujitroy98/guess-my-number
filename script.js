6e993333586337b9c0d1d5b1eb29870f84df8d23
Sujitroy98 <115344822+Sujitroy98@users.noreply.github.com>
2023-09-12 11:51:45 +0530

    if else statements added

--- script.js
+++ script.js
@@ -27,8 +27,52 @@ const displayWidth = function (width){
   document.querySelector('.number').style.width = width;
 }
 
+// ! click event listener
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
+  // ! NO INPUT
+  if (!guess) {
+    
+    displayMessage(`🚫 wrong input`);
 
+    // ! guess is equal
+  } else if (guess === secretNumber) {
+    
+    displayMessage(` 😁 Correct number!`);
+    
+    displayNumber(secretNumber);
+    
+
+    
+    displayBackgroundColor('#60b347');
+    displayWidth('30rem')
+    
+    if(score > highscore) {
+      highscore = score ;
+      displayHighscore(highscore);
+      
+    }
+  }
+
+    //! guess is not equal
+    else if (guess !== secretNumber){
+      if (score > 1) {
+        
+        displayMessage(guess > secretNumber ? 'Number too high!': 'Number too low!');
+        score--;
+        displayScore(score);
+        
+      }
+      else {
+        
+        displayMessage(`😭You lost the Game!`);
+        displayScore(0);
+        
+      }
+    }
+  })
 document.querySelector('.again').addEventListener('click', function(){
     score = '20';
     secretNumber = Math.trunc(Math.random() * 20) + 1;
